1e465c6a5e1e74f15dfa36a2427307727a445758
Josh Patterson <josh@ntdls.com>
2024-02-01 09:15:55 -0500

    Moved code.

---
+++ NTDLS.Katzebase.SQLServerMigration/Classes/SelectedSqlItem.cs
@@ -0,0 +1,16 @@
+﻿namespace NTDLS.Katzebase.SQLServerMigration.Classes
+{
+    internal class SelectedSqlItem
+    {
+        public string Schema { get; set; }
+        public string Table { get; set; }
+        public ListViewItem ListItem { get; set; }
+
+        public SelectedSqlItem(ListViewItem item, string schema, string name)
+        {
+            ListItem = item;
+            Schema = schema;
+            Table = name;
+        }
+    }
+}
---
+++ NTDLS.Katzebase.SQLServerMigration/Classes/TabelWorkerThreadParam.cs
@@ -0,0 +1,18 @@
+﻿namespace NTDLS.Katzebase.SQLServerMigration.Classes
+{
+    internal class TabelWorkerThreadParam
+    {
+        public SelectedSqlItem Item { get; set; }
+        public int TargetServerPort { get; set; }
+        public string TargetServerHost { get; set; }
+        public string TargetServerSchema { get; set; }
+
+        public TabelWorkerThreadParam(string targetServerHost, int targetServerPort, string targetServerSchema, SelectedSqlItem item)
+        {
+            TargetServerHost = targetServerHost;
+            TargetServerPort = targetServerPort;
+            TargetServerSchema = targetServerSchema;
+            Item = item;
+        }
+    }
+}
